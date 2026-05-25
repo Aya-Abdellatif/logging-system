94e2ed5e6e8dc23f498c8ad7036be972c1189aa4
Aya Abdellatif <ayaabdllatif112@gmail.com>
2026-05-25 14:18:58 +0300

    Add developers.model.js

---
+++ backend/src/developers/developer.model.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { v4 as uuidv4 } from 'uuid';
+
+const developerSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters'],
+      select: false,
+    },
+    apiKey: {
+      type: String,
+      unique: true,
+      default: () => uuidv4(),
+    },
+  },
+  { timestamps: true }
+);
+
+developerSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) 
+    return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+developerSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const Developer = mongoose.model('Developer', developerSchema);
+
+export default Developer;
