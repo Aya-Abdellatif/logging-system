3fe8b9322450bd09d64c12e5fa6606896f6a4882
Aya Abdellatif <ayaabdllatif112@gmail.com>
2026-05-25 15:08:03 +0300

    Setup developer controller

---
+++ backend/src/developers/developer.controllers.js
@@ -0,0 +1,30 @@
+import User from "./users.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const registerDeveloperController = async (request, response, next) => {
+    try {
+
+    }
+    catch (error) {
+        return next(error);
+    }
+}
+
+export const loginDeveloperController = async (request, response, next) => {
+    try {
+    
+    }
+    catch (error) {
+        return next(error);
+    }
+}
+
+export const logoutDeveloperController = async (request, response, next) => {
+    try {
+    
+    }
+    catch (error) {
+        return next(error);
+    }
+}
--- backend/src/developers/developer.router.js
+++ backend/src/developers/developer.router.js
@@ -1,8 +1,9 @@
+import { registerDeveloperController, loginDeveloperController, logoutDeveloperController } from "./developer.controllers";
 import express from "express";
 const router = express.Router();
 
-router.post("/register", );
-router.post("/login", );
-router.post("/logout", );
+router.post("/register", registerDeveloperController);
+router.post("/login", loginDeveloperController);
+router.post("/logout", logoutDeveloperController);
 
 export default router;
