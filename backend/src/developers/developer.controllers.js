304a01b0ed75b94d771617e275d991955683f4be
Aya Abdellatif <ayaabdllatif112@gmail.com>
2026-05-25 15:11:24 +0300

    Add getMe controller

--- backend/src/developers/developer.controllers.js
+++ backend/src/developers/developer.controllers.js
@@ -2,6 +2,7 @@ import User from "./users.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+// POST /api/developers/register
 export const registerDeveloperController = async (request, response, next) => {
     try {
 
@@ -11,20 +12,31 @@ export const registerDeveloperController = async (request, response, next) => {
     }
 }
 
+// POST /api/developers/login
 export const loginDeveloperController = async (request, response, next) => {
     try {
-    
+
     }
     catch (error) {
         return next(error);
     }
 }
 
+// POST /api/developers/logout
 export const logoutDeveloperController = async (request, response, next) => {
     try {
-    
+
     }
     catch (error) {
         return next(error);
     }
 }
+
+// GET /api/developers/me
+export const getMe = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err);
+    }
+};
