0202097e1b8303e420421eb0146c545813aa7934
Aya Abdellatif <ayaabdllatif112@gmail.com>
2026-07-09 22:47:14 +0300

    Add a request logger

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,11 @@ dotenv.config();
 
 const server = express();
 
+server.use((req, res, next) => {
+  console.log(req.method, req.originalUrl);
+  next();
+});
+
 server.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
